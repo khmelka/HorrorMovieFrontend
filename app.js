8a617509430644b6be7c16178beebf99a13e8716
Nelly <nkhmeleva@yahoo.com>
2019-09-18 01:10:41 -0600

    sorting works

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const movieAPI = "http://localhost:3000/movies"
   const likeAPI = "http://localhost:3000/likes"
 
-function createCard(element){
 
   const body = document.querySelector('body')
 
+  const mainContainer = document.createElement('div')
+  mainContainer.className='mainContainer'
+  body.appendChild(mainContainer)
+
+function createCard(element){
+
+  // const body = document.querySelector('body')
+
   const container = document.createElement('div')
   container.className = 'container'
-  body.appendChild(container)
+  mainContainer.appendChild(container)
 
   const card = document.createElement('div')
   card.className = 'card'
@@ -63,7 +70,7 @@ function createCard(element){
   emoji.addEventListener("click", function (){
       const click = parseInt(likes.innerText)
       likes.innerText = parseInt(click+1)
-  
+  console.log("like", event)
       fetch (likeAPI, {
       method: 'POST',
       headers: {
@@ -102,9 +109,27 @@ fetch(movieAPI)
 .then (resp => resp.json())
 .then (json => renderInfo(json))
 
+
+
+function deleteChild() { 
+  let e = document.querySelector(".mainContainer"); 
+  let first = e.firstElementChild; 
+  while (first) { 
+      first.remove(); 
+      first = e.firstElementChild; 
+  } 
+} 
+
+
+// function createCard(){
+
+// }
+
+
 function renderInfo(json){
   json.forEach(element => {
       // console.log(element)
+      
       createCard(element)
 
   })
@@ -113,29 +138,76 @@ function renderInfo(json){
 
 //selecting from dropdown menu
 
-  const menu = document.getElementById('menu')
+  // const menu = document.getElementById('menu')
 
-  function yearCount(a, b) {
+
+  const liked = document.getElementById('liked')
+  const newest = document.getElementById('newest')
+  const oldest = document.getElementById('oldest')
+  const newcard = document.getElementById('newcard')
+
+  function likeCount(a,b) {
+    return b.likes.length - a.likes.length
+  }
+
+  function yearCountNew(a, b) {
+    return b.release_year - a.release_year
+  }
+
+  function yearCountOld(a, b) {
     return a.release_year - b.release_year
   }
 
 
-  menu.addEventListener("change", function(){
-    if (menu.value == 1){
-      console.log(json.sort(yearCount))
-    }
+  liked.addEventListener("click", function(){
+    deleteChild()
+    renderInfo(json.sort(likeCount))
+  })
+
+  newest.addEventListener("click", function(){
+    deleteChild()
+    renderInfo(json.sort(yearCountNew))
+  })
+
+  oldest.addEventListener("click", function(){
+    deleteChild()
+    renderInfo(json.sort(yearCountOld))
+  })
+
+  document.querySelector(".formMainContainer").style.display = "none"
+
+  newcard.addEventListener("click", function(){
+    document.querySelector(".formMainContainer").style.display = "block"
+    // createCard()
+  })
+
+  document.getElementById("button").addEventListener("click", function(){
+    console.log("yes")
+    createCard()
+  })
+
+
+  // menu.addEventListener("change", function(){
+  //   // sort by 
+
+  //   if (menu.value == 1){
+  //     deleteChild()
+  //     renderInfo(json.sort(likeCount))
+  //   }
 
     
-    else if (menu.value == 2){
-        console.log("2")
+  //   if (menu.value == 2){
+  //     deleteChild()
+  //     renderInfo(json.sort(yearCountNew))
 
 
-    }
-    else if (menu.value == 3){
-      console.log("3")
+  //   }
+  //   if (menu.value == 3){
+  //     deleteChild()
+  //     renderInfo(json.sort(yearCountOld))
 
-    }
-  })//closing eventListener
+  //   }
+  // })//closing eventListener on menu
 
 
 
